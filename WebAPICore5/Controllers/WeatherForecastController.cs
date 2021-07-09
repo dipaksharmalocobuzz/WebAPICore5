1ae7a3be4ca4cb593ed6275feb85c82fcf37e1f5
Dipak Sharma <dipak.sharma@locobuzz.com>
2021-07-09 13:27:34 +0530

    update

--- WebAPICore5/Controllers/WeatherForecastController.cs
+++ WebAPICore5/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebAPICore5.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Mumbai", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Delhi", "Mumbai", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
